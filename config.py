7e13c85d33d957124d2e8e369bc647075e04745b
nikola pavkovic <nikola.pavkovic@ieee.org>
2022-02-28 08:45:25 +0100

    added prediction model generator and test script without predicted rotations

--- config.py
+++ config.py
@@ -6,17 +6,14 @@ BASE_PATH = "C:/Users/Nikola/Desktop/skin_lesions"
 IMAGES_PATH = os.path.sep.join([BASE_PATH, "images"])
 ANNOTS_PATH = os.path.sep.join([BASE_PATH, "annotations_modified.csv"])
 
-# define the path to the base output directory
 BASE_OUTPUT = "C:/Users/Nikola/Desktop/skin_lesions/outputs"
-# define the path to the output serialized model, model training plot,
-# and testing image filenames
+
 MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "detector.h5"])
 PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
 TEST_FILENAMES = os.path.sep.join([BASE_OUTPUT, "test_images.txt"])
 
 
-# initialize our initial learning rate, number of epochs to train
-# for, and the batch size
+
 INIT_LR = 1e-4
 NUM_EPOCHS = 3
 BATCH_SIZE = 32
